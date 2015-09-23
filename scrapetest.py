73e53f41bf2d8d0856dc1062e3613fe94593ce09
yaskyj <yaskyj@yahoo.com>
2015-09-22 20:50:13 -0500

    Add error handling to scrapetest.py

--- scrapetest.py
+++ scrapetest.py
@@ -1,5 +1,19 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-html = urlopen("http://pythonscraping.com/pages/page1.html")
-bsObj = BeautifulSoup(html.read(), 'html.parser')
-print(bsObj.h1)
+def getTitle(url):
+  try:
+    html = urlopen(url)
+  except HTTPError as e:
+    return None
+  try:
+    bsObj = BeautifulSoup(html.read(), 'html.parser')
+    title = bsObj.body.h1
+  except AttributeError as e:
+    return None
+  return title
+
+title = getTitle("http://pythonscraping.com/pages/page1.html")
+if title == None:
+  print("Title could not be found")
+else:
+  print(title)
